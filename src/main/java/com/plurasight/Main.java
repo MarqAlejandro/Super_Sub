d9bdde7432855b0d54605fb495c3b2a0a84126ce
MarqAlejandro <alejandromarq3@gmail.com>
2025-05-26 20:36:06 -0700

    Initial Commit
    creation of the project

--- src/main/java/com/plurasight/Main.java
+++ src/main/java/com/plurasight/Main.java
@@ -3,6 +3,6 @@ package com.plurasight;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, User, Welcome to Super(Sub)!");
+        System.out.println("Hello, User, Welcome to super(sub)!");
     }
 }
